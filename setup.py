678daec81bbb355f29a830c728c0a0ae6f575782
Carlos A. Michelén Ströfer <cmichel@sandia.gov>
2021-06-03 20:18:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='DAFI',
-    version='1.0.1',
+    version='1.0.2',
     packages=setuptools.find_packages(),
     scripts=['bin/dafi'],
     license='Apache 2.0',
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/xiaoh/DAFI',
-    download_url="https://github.com/xiaoh/DAFI/archive/refs/tags/1.0.0.tar.gz",
+    download_url="https://github.com/xiaoh/DAFI/archive/refs/tags/1.0.2.tar.gz",
     keywords=["Data Assimilation", "Field Inversion"],
     install_requires=[
         'numpy',
